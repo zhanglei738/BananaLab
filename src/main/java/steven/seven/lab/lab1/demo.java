51f10f6b4ce0b37aa7d587fec1174579a93e40dc
Seven <seven@diqigan.cn>
2018-07-04 14:54:55 +0800

    Convert Tab to Space

--- src/main/java/steven/seven/lab/lab1/demo.java
+++ src/main/java/steven/seven/lab/lab1/demo.java
@@ -4,24 +4,24 @@ import org.junit.Test;
 
 public class demo extends com.bigbanana.lab.lab1.CalculateSum{
 
-	@Override
-	public long sum(Integer... numbers){
-		long sum = 0;
+    @Override
+    public long sum(Integer... numbers){
+        long sum = 0;
 
-      	if (numbers != null && numbers.length > 0) {
-       		for (Integer number : numbers) {
-      			if (number != null) {
-      				sum += number;
-      			}
-      		}
-      	}
+        if (numbers != null && numbers.length > 0) {
+            for (Integer number : numbers) {
+                if (number != null) {
+                    sum += number;
+                }
+            }
+        }
 
-		return sum;
-	}
+        return sum;
+    }
 
-	@Test
-	public void myTest(){
-		demo demo = new demo();
-		super.test(demo);
-	}
+    @Test
+    public void myTest(){
+        demo demo = new demo();
+        super.test(demo);
+    }
 }
