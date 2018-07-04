998e0098296283e9ef6317934ea201d7c71f208d
大蕉 <zhao.yang5683@gmail.com>
2018-07-04 23:11:32 +0800

    实现代码

--- src/main/java/com/bigbanana/lab/lab1/dajiao/demo.java
+++ src/main/java/com/bigbanana/lab/lab1/dajiao/demo.java
@@ -3,15 +3,28 @@ package com.bigbanana.lab.lab1.dajiao;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class demo extends com.bigbanana.lab.lab1.CalculateSum{
 
 
 	@Override
 	public  long sum(Integer... numbers){
-		return numbers.length;
+
+		return Stream.of(
+		Optional.ofNullable(numbers)
+				.orElse(new Integer[0])
+		).filter(Objects::nonNull)
+				.map( x -> x.longValue())
+				.reduce( (x1,x2)->x1+x2)
+		.orElse(0L);
+
+
 	}
 
-	//@Test
+	@Test
 	public void myTest(){
 		demo demo = new demo();
 		super.test(demo);
