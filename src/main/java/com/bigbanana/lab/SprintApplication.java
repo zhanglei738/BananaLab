34e7d9bc7e8668ab1da44a880c5c8635f711954b
大蕉 <zhao.yang5683@gmail.com>
2018-07-14 14:50:25 +0800

    完成 lab4 编写。

--- src/main/java/com/bigbanana/lab/SprintApplication.java
+++ src/main/java/com/bigbanana/lab/SprintApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 public class SprintApplication {
