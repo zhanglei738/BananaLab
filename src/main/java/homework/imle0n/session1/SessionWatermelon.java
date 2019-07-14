2c84ff47d4023dc669d0331f599c9eee9f450142
iMLe0n <imle0n106@gmail.com>
2019-07-14 23:05:18 +0800

    first homework

---
+++ src/main/java/homework/imle0n/session1/SessionWatermelon.java
@@ -0,0 +1,55 @@
+package homework.imle0n.session1;
+
+import java.util.Arrays;
+
+/**
+ * @author iMLe0n
+ * @version 1.0.0
+ * @date 19-7-14 下午10:58
+ */
+public class SessionWatermelon {
+    public static int MaxNum = 50;
+
+    public static void main(String[] args) {
+        int[] nums1 = {-1, 0, 10, 51};
+        int[] sell = sell(nums1);
+        System.out.println(Arrays.toString(sell));
+    }
+
+    public static int[] sell(int[] buyNum) {
+        if (buyNum == null || buyNum.length == 0) {
+            return new int[0];
+        }
+
+        int[] result = new int[buyNum.length];
+        int buyNumCount = 0;
+
+        for (int i = 0; i < buyNum.length; i++) {
+            int currentBuyNUm = buyNum[i];
+            int sellNum = 0;
+            try {
+                sellNum = sell0(currentBuyNUm);
+            } catch (Exception e) {
+                System.err.println(e.getMessage() + " --------> " + currentBuyNUm);
+                sellNum = 0;
+            }
+
+            result[i] = sellNum;
+            buyNumCount += sellNum;
+        }
+
+        System.out.println("iMLe0n总卖出" + buyNumCount + "个");
+        return result;
+    }
+
+    private static int sell0(int buyNum) throws Exception {
+        if (buyNum < 0) {
+            throw new Exception("售卖数不能为负数!");
+        }
+        if (buyNum > MaxNum) {
+            return MaxNum;
+        }
+        return buyNum;
+    }
+
+}
