2c7c51f3f6cba1f5a8d47521234a83bd222a0148
zeng ge <sweetifan@163.com>
2019-07-15 19:12:47 +0800

    修复一些bug;增加一个自己实现的数组打印功能

--- src/main/java/homework/sweet/session1/BuyWatermelon.java
+++ src/main/java/homework/sweet/session1/BuyWatermelon.java
@@ -1,7 +1,9 @@
 package homework.sweet.session1;
 
-import java.util.Arrays;
-
+/**
+ * 一个卖西瓜的操作
+ * @author sweet
+ */
 public class BuyWatermelon {
     public static void main(String[] args) {
         int[] testNum1 = {-2,0,2,30,51,100};
@@ -9,13 +11,17 @@ public class BuyWatermelon {
 //        int[] testNum3 = new int[10];
 
         int[] resultArray1 = sell(testNum1);
-        System.out.println(Arrays.toString(resultArray1));
+        printIntArr(resultArray1);
     }
 
     public static int[] sell(int[] buyNum){
         int count = 0;
-        if (buyNum.length == 0) return new int[0];
+        //先判空再判断长度，否则会出现 NullPointException
         if (buyNum == null) return new int[0];
+        if (buyNum.length == 0) return new int[0];
+
+        //用一个新的数组来存放处理后的结果，不影响原来的数据
+        int[] resultBuyNum = new int[buyNum.length];
 
         for (int i = 0; i < buyNum.length; i++) {
             int currentNum = buyNum[i];
@@ -26,12 +32,12 @@ public class BuyWatermelon {
                 tmpResult = 0;
                 e.printStackTrace();
             }
-            buyNum[i] = tmpResult;
+            resultBuyNum[i] = tmpResult;
             count += tmpResult;
         }
 
         System.out.println("testNum1 一共卖出：" + count + " 个西瓜。");
-        return  buyNum;
+        return  resultBuyNum;
     }
 
     public static int sell0(int tmp) throws Exception{
@@ -42,5 +48,22 @@ public class BuyWatermelon {
             tmp = 50;
         }
         return tmp;
+    }
+
+    /**
+     * 实现一个数组打印功能
+     * @param arr
+     */
+    public static void printIntArr(int[] arr){
+
+        System.out.print("[");
+        if (arr == null || arr.length == 0)
+            System.out.print("]");
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 2)
+                System.out.print(",");
+        }
+        System.out.println("]");
     }
 }
