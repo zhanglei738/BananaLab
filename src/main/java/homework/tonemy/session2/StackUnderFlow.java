ea8bd3f2f5b04b68c0b5416ca4b4030960690d62
tonemy <1006045923@qq.com>
2019-07-19 13:18:22 +0800

    tonemy's second homework

---
+++ src/main/java/homework/tonemy/session2/StackUnderFlow.java
@@ -0,0 +1,34 @@
+
+public class StackUnderFlow extends RuntimeException {
+
+	/**
+	 * Õ»µÄÏÂÒç
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StackUnderFlow() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackUnderFlow(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackUnderFlow(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackUnderFlow(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StackUnderFlow(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
